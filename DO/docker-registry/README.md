4c6c9f6a1a8669cb8e3643bbbe6cfcdf2524069e
stopsopa <grytysek@gmail.com>
2019-12-24 04:21:48 +0000

    dns

--- DO/docker-registry/README.md
+++ DO/docker-registry/README.md
@@ -34,9 +34,9 @@ Check now if everything is working as expected by visiting one of links:
 
 * `https://docker.domain.com/v2/_catalog`
 
-* `https://docker.phaseiilabs.com/v2/{your-repo-name-here}/tags/list`
+* `https://docker.domain.com/v2/{your-repo-name-here}/tags/list`
 
-* `https://docker.phaseiilabs.com/v2/twlphaseii/node-jenkins-docker/manifests/latest` <-- to download and see the metadata for the tag
+* `https://docker.domain.com/v2/twlphaseii/node-jenkins-docker/manifests/latest` <-- to download and see the metadata for the tag
 
 <br />
 
--- DO/docker-registry/install.sh
+++ DO/docker-registry/install.sh
@@ -2,34 +2,52 @@
 
 . ./Infrastructure/Registry/.env && echo ${DOCKERREGISTRY}
 
-yum install -y httpd-tools
+#yum install -y httpd-tools
+#
+#echo "Installing bare minimum soft"
+#yum install -y firewalld httpd-tools \
+#    && systemctl start firewalld \
+#    && systemctl enable firewalld \
+#    && service docker stop \
+#    && service docker start
+#    # restart docker after running firewall because docker might not see firewall without it
+#
+#yum install -y epel-release \
+#    && yum install -y python-pip gcc \
+#    && pip install docker-compose requests urllib3 pyOpenSSL --force --upgrade
+#yum upgrade -y python*
+#
+#CMD="$(cat <<EOF
+#docker run -it --rm --name certbot -p 80:80 -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt certbot/certbot certonly --standalone -d ${DOCKERREGISTRY} --preferred-challenges http --agree-tos -n -m ${CERTBOT_EMAIL}
+#EOF
+#)"
+#
+#printf "\n\n$CMD\n\n";
+#
+#$CMD;
+#
+#htpasswd -cBb ~/.htpasswd ${PASSWD_USER} ${PASSWD_PASSWORD}
+#
+#echo "Running docker registry image"
+#cd ./Infrastructure/Registry/ && docker-compose up -d
 
-echo "Installing bare minimum soft"
-yum install -y firewalld httpd-tools \
-    && systemctl start firewalld \
-    && systemctl enable firewalld \
-    && service docker stop \
-    && service docker start
-    # restart docker after running firewall because docker might not see firewall without it
 
-yum install -y epel-release \
-    && yum install -y python-pip gcc \
-    && pip install docker-compose requests urllib3 pyOpenSSL --force --upgrade
-yum upgrade -y python*
+cat << EOF
 
-CMD="$(cat <<EOF
-docker run -it --rm --name certbot -p 80:80 -v /etc/letsencrypt:/etc/letsencrypt -v /var/lib/letsencrypt:/var/lib/letsencrypt certbot/certbot certonly --standalone -d ${DOCKERREGISTRY} --preferred-challenges http --agree-tos -n -m ${CERTBOT_EMAIL}
+To test repository try few links in browser:
+
+https://$DOCKERREGISTRY/v2/
+
+https://$DOCKERREGISTRY/v2/_catalog
+
+https://$DOCKERREGISTRY/v2/{your-repo-name-here}/tags/list
+
+https://$DOCKERREGISTRY/v2/twlphaseii/node-jenkins-docker/manifests/latest
+  # to download and see the metadata for the tag
+
 EOF
-)"
 
-printf "\n\n$CMD\n\n";
 
-$CMD;
-
-htpasswd -cBb ~/.htpasswd ${PASSWD_USER} ${PASSWD_PASSWORD}
-
-echo "Running docker registry image"
-cd ./Infrastructure/Registry/ && docker-compose up -d
 
 # Or manually using docker run
 # sudo docker run -d --name registry --restart=always \
