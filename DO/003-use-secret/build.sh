dedba439c63da153d2a010178b78cebe58c6748d
stopsopa <grytysek@gmail.com>
2019-12-25 20:52:31 +0000

    secret & kuber using custom repository

---
+++ DO/003-use-secret/build.sh
@@ -0,0 +1,24 @@
+
+
+THISFILE=${BASH_SOURCE[0]}
+DIR="$( cd "$( dirname "${THISFILE}" )" && pwd -P )"
+
+source "$DIR/.env";
+
+if [ "$1" = "list" ]; then
+
+    curl https://$REGISTRY/v2/$APP/tags/list -u "$USER:$PASSWORD"
+
+    exit $?;
+fi
+
+if [ "$1" != "" ]; then
+
+    VER="$1";
+fi
+
+cd docker
+docker build -t $APP:$VER .
+docker tag $APP:$VER $REGISTRY/$APP:$VER
+docker push $REGISTRY/$APP:$VER
+cd ..
